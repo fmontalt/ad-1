66fcedab12456eee79b92093ce0768f9a88e3b95
luisvelasco17 <luis.velasco@institutoserpis.org>
2017-11-20 13:33:27 +0100

    Cambios pendientes en CArticulo; añadido ComboBoxHelper en CSerpisAd (probado en CComboBox)

---
+++ CArticulo/CArticulo/ArticuloWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CArticulo
+{
+    public partial class ArticuloWindow : Gtk.Window
+    {
+        public ArticuloWindow(Articulo articulo) :
+                base(Gtk.WindowType.Toplevel) {
+            this.Build();
+
+            entryNombre.Text = articulo.Nombre;
+            spinButtonPrecio.Value = Convert.ToDouble(articulo.Precio);
+            entryCategoria.Text = articulo.Categoria.ToString();
+
+            saveAction.Activated += delegate {
+                articulo.Nombre = entryNombre.Text;
+                articulo.Precio = Convert.ToDecimal(spinButtonPrecio.Value);
+                articulo.Categoria = long.Parse(entryCategoria.Text);
+                ArticuloDao.Save(articulo);
+                Destroy();
+            };
+        }
+    }
+}
---
+++ CArticulo/CArticulo/MainWindow.cs
@@ -0,0 +1,39 @@
+﻿using Gtk;
+using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+using CArticulo;
+using Serpis.Ad;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel) {
+        Build();
+		App.Instance.Connection = new MySqlConnection("server=localhost;database=dbprueba;user=root;password=sistemas");
+		App.Instance.Connection.Open();
+
+        TreeViewHelper.Fill(treeView, ArticuloDao.SelectAll);
+
+        newAction.Activated += delegate {
+            Articulo articulo = new Articulo();
+            new ArticuloWindow(articulo);
+        };
+
+        editAction.Activated += delegate {
+            object id = TreeViewHelper.GetId(treeView);
+            Articulo articulo = ArticuloDao.Load(id);
+            new ArticuloWindow(articulo);
+        };
+
+        refreshAction.Activated += delegate {
+			TreeViewHelper.Fill(treeView, ArticuloDao.SelectAll);
+		};
+	}
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a) {
+        App.Instance.Connection.Close();        
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
